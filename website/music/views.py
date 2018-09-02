1726182eb1db2eb970fd0ce790c02f9fc1c6efeb
akash1309 <akash.goyal1309@gmail.com>
2018-09-02 23:37:31 +0530

    addition of column is_favorite in models and adding of url of favorite

--- website/music/views.py
+++ website/music/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Album
 from django.http import Http404
 
@@ -11,9 +11,6 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
